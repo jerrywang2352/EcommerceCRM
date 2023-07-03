00ce840303043ae59521afa9cdb5cfc656325d1d
Jerry Wang <jerry.wang2352@gmail.com>
2023-07-03 17:19:45 -0400

    Initialize navbar, products, home components

--- src/storefront/src/App.js
+++ src/storefront/src/App.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './components/Home'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        "Hello World"
-      </div>
-    )
+      <BrowserRouter>
+        <Routes>
+          <Route path = '/' element={<Home/>} />
+        </Routes>
+      </BrowserRouter>
+    );
   }
 }
 
-export default App
+export default App;

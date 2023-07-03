77976c2993b44fccfc21c1701dfce79858c75bcf
Jerry Wang <jerry.wang2352@gmail.com>
2023-07-03 17:39:41 -0400

    Initial navbar creation and styling

--- src/storefront/src/components/Home.js
+++ src/storefront/src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Navbar from './Navbar';
-import Products from './Products';
+import '../css/Home.css'
+import Navbar from './Navbar'
+import Products from './Products'
 
 const Home = () => {
     return (
-        <div>
+        <div className='wrapper'>
             <Navbar />
             <Products />
         </div>
